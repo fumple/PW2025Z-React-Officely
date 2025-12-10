02239c93654327a3cd03f210dfcf8b7e1f6f5afd
Fumple <me@fumple.pl>
2025-12-10 17:00:59 +0100

    Formatted the code

--- frontend-web/eslint.config.js
+++ frontend-web/eslint.config.js
@@ -1,21 +1,21 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
-import eslintConfigPrettier from 'eslint-config-prettier'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-      eslintConfigPrettier
+      eslintConfigPrettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
@@ -27,12 +27,15 @@ export default defineConfig([
       "no-restricted-imports": [
         "error",
         {
-          "patterns": [{
-            "regex": "^@mui/[^/]+$",
-            message: "Imports like `import { Button } from '@mui/material'` are not allowed, please use `import Button from '@mui/material/Button'`"
-          }]
-        }
-      ]
-    }
+          patterns: [
+            {
+              regex: "^@mui/[^/]+$",
+              message:
+                "Imports like `import { Button } from '@mui/material'` are not allowed, please use `import Button from '@mui/material/Button'`",
+            },
+          ],
+        },
+      ],
+    },
   },
-])
+]);
