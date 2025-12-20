9af4bcde00e34da448a7d8a9d45ec1c6743e6b4b
paula <paulamat1>
2025-12-20 18:06:04 +0100

    Initial Spring Boot backend setup

---
+++ officely-backend/src/test/java/com/officely/backend/OfficelyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.officely.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficelyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
