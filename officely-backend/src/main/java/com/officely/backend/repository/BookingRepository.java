d14713ebd5910feeb8b940809ae7cd31c83b14cd
Fumple <me@fumple.pl>
2026-02-01 15:31:55 +0100

    [backend] Permissions are now checked correctly in listing bookings + mapping fixes

--- officely-backend/src/main/java/com/officely/backend/repository/BookingRepository.java
+++ officely-backend/src/main/java/com/officely/backend/repository/BookingRepository.java
@@ -1,19 +1,23 @@
 package com.officely.backend.repository;
 
 import com.officely.backend.entity.BookingEntity;
-import com.officely.backend.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-import java.awt.print.Book;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface BookingRepository extends JpaRepository<BookingEntity, Long>{
-    Optional<List<BookingEntity>> findByUserId(Long userId);
     Optional<BookingEntity> findByIdAndUserId(Long bookingId, Long userId);
     Page<BookingEntity> findByUserIdOrderByStartDateDesc(Long userId, Pageable pageable);
+
+    @Query("select distinct b from BookingEntity b left join OfficeEntity o ON o.id = b.office.id" +
+            " left join OfficeMemberEntity m ON m.office.id = o.id" +
+            " where o.owner.id = :userId OR m.user.id = :userId")
+    Page<BookingEntity> getByAdminUserId(long userId, Pageable pageable);
+
+    Page<BookingEntity> getByOfficeId(long officeId, Pageable pageable);
 }
