e58f7ee09e2cdabe8495093e4ee13de7532a64ef
Fumple <me@fumple.pl>
2026-02-01 17:42:00 +0100

    [frontend-web] Forgot to apply formatting

--- frontend-web/vite.config.ts
+++ frontend-web/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/app/',
+  base: "/app/",
   plugins: [react()],
   resolve: {
     dedupe: ["react", "react-dom"],
